41cf19cb34e395784dda566f6910845e558aa065
looooo <sppedflyer@gmail.com>
2024-07-10 01:00:24 +0200

    some tests

--- prepare.py
+++ prepare.py
@@ -163,8 +163,8 @@ def prepare_smesh():
     shutil.copytree('extra/MeshVSLink',
                     'src/SMESH/src/MeshVSLink', dirs_exist_ok=True)
 
-    # Copy MEFISTO2 trte.c source
-    shutil.copyfile('extra/MEFISTO2/trte.c', 'src/SMESH/src/MEFISTO2/trte.c')
+    # # Copy MEFISTO2 trte.c source
+    # shutil.copyfile('extra/MEFISTO2/trte.c', 'src/SMESH/src/MEFISTO2/trte.c')
 
 
 def prepare_netgen_plugin():
